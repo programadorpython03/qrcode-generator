21143868b87a845c8663f3098c895346c922fc60
programadorpython03 <programadorpython03@gmail.com>
2025-03-12 11:08:29 -0300

    Adição da função para adicionar logo no QR Code e cores personalizadas.

--- qrcode_app/models.py
+++ qrcode_app/models.py
@@ -6,10 +6,15 @@ class QRCode(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     link = models.URLField(blank=True, null=True)
     file = models.FileField(upload_to='qrcodes/files/', blank=True, null=True)
-    is_temporary = models.BooleanField(default=False, blank=True, null=True)
-    qrcode_image = models.ImageField(upload_to='qrcodes/images/', blank=True, null=True)
+    is_temporary = models.BooleanField(default=False)
     expiration_date = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    qrcode_image = models.ImageField(upload_to='qrcodes/images/', blank=True, null=True)
+
+    # Campos para personalização
+    fill_color = models.CharField(max_length=7, default='#000000')  # Cor do QR code (hexadecimal)
+    back_color = models.CharField(max_length=7, default='#FFFFFF')  # Cor de fundo (hexadecimal)
+    logo = models.ImageField(upload_to='qrcodes/logos/', blank=True, null=True)  # Logo opcional
 
     def is_expired(self):
         if self.is_temporary and self.expiration_date:
--- qrcode_app/views.py
+++ qrcode_app/views.py
@@ -1,16 +1,20 @@
-import qrcode
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import QRCode
+import qrcode
 from io import BytesIO
 from django.core.files.base import ContentFile
 from datetime import datetime, timedelta
+from PIL import Image, ImageDraw
 
 def generate_qrcode(request):
     if request.method == 'POST':
         link = request.POST.get('link')
         file = request.FILES.get('file')
         is_temporary = request.POST.get('is_temporary') == 'on'
+        fill_color = request.POST.get('fill_color', '#000000')  # Cor do QR code
+        back_color = request.POST.get('back_color', '#FFFFFF')  # Cor de fundo
+        logo = request.FILES.get('logo')  # Logo opcional
         expiration_days = request.POST.get('expiration_days')
 
         # Define expiration_date como None se expiration_days não for fornecido
@@ -22,15 +26,38 @@ def generate_qrcode(request):
             link=link,
             file=file,
             is_temporary=is_temporary,
-            expiration_date=datetime.now() + timedelta(days=expiration_days) if is_temporary else None
+            expiration_date=datetime.now() + timedelta(days=expiration_days) if is_temporary else None,
+            fill_color=fill_color,
+            back_color=back_color,
+            logo=logo
         )
         qr.save()
 
         # Gerar o QR code
         qr_data = link if link else qr.file.url
-        qr_img = qrcode.make(qr_data)
+        qr_img = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=10,
+            border=4,
+        )
+        qr_img.add_data(qr_data)
+        qr_img.make(fit=True)
+
+        # Criar a imagem do QR code
+        img = qr_img.make_image(fill_color=fill_color, back_color=back_color).convert('RGB')
+
+        # Adicionar o logo (se fornecido)
+        if logo:
+            logo_img = Image.open(logo)
+            logo_size = 50  # Tamanho do logo
+            logo_img = logo_img.resize((logo_size, logo_size), Image.Resampling.LANCZOS)
+            pos = ((img.size[0] - logo_size) // 2, (img.size[1] - logo_size) // 2)
+            img.paste(logo_img, pos)
+
+        # Salvar a imagem do QR code
         buffer = BytesIO()
-        qr_img.save(buffer, format="PNG")
+        img.save(buffer, format="PNG")
         qr.qrcode_image.save(f'{qr.id}.png', ContentFile(buffer.getvalue()), save=False)
         qr.save()
 
